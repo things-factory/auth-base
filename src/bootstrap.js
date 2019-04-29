83b330b3d40b47ec3eccfbe15d8da49ce6d30d5e
heartyoh <heartyoh@hatiolab.com>
2019-04-29 11:15:31 +0800

    Initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,22 @@
+import { store } from '@things-factory/shell'
+import { auth } from './auth'
+import reducerAuth from './reducers/auth'
+import { updateAuthenticated, updateUser } from './actions/auth'
+
+export default function bootstrap() {
+  store.addReducers({
+    auth: reducerAuth
+  })
+
+  auth.on('signin', accessToken => {
+    store.dispatch(updateAuthenticated(true))
+  })
+
+  auth.on('signout', () => {
+    store.dispatch(updateAuthenticated(false))
+  })
+
+  auth.on('profile', profile => {
+    store.dispatch(updateUser(profile))
+  })
+}
