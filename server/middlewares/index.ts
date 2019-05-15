8a250a8e1ff81d58ed8af41f28862c927fe38a54
leemyunggyu <jaylee@hatiolab.com>
2019-05-15 18:16:37 +0900

    change serve:dev script

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -3,6 +3,5 @@ import { authMiddleware } from './auth-middleware'
 ;(authMiddleware as any).unless = unless
 
 /* 아래의 path로 시작하는 경우에만, authcheck를 한다. */
-;(authMiddleware as any).unless({ path: [/^(?!.graphql|.file|.uploads|.authcheck).*$/] })
 
-export const middlewares = [authMiddleware]
+export const middlewares = [(authMiddleware as any).unless({ path: [/^(?!.graphql|.file|.uploads|.authcheck).*$/] })]
