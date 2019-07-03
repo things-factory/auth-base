9db68f5ab1916d062fe0c3f9cd2e31623efb0898
Hearty Oh <heartyoh@hatiolab.com>
2019-07-03 12:40:39 +0900

    todo comments

--- server/entities/user.ts
+++ server/entities/user.ts
@@ -13,6 +13,7 @@ import {
 } from 'typeorm'
 import { Role } from './role'
 
+/* TODO SECRET KEY를 변경할 수 있는 방법을 제시해야 한다. */
 const SECRET = '0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95'
 
 @Entity('users')
--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -2,6 +2,7 @@ import unless from 'koa-unless'
 import { authMiddleware } from './auth-middleware'
 ;(authMiddleware as any).unless = unless
 
+/* TODO authcheck 화이트리스트를 모듈에서 추가할 수 있는 방법을 제시해야 한다. */
 process.on('bootstrap-module-middleware' as any, app => {
   /* 아래의 path로 시작하는 경우에만, authcheck를 한다. */
   let unlessOption = { path: [/^(?!.graphql|.file|.uploads|.authcheck|.change_pass).*$/] }
