1274aeceff466a479938074625fa3e0a679e2f64
leemyunggyu <jaylee@hatiolab.com>
2019-08-13 17:47:11 +0800

    v0.1.39

--- server/graphql/resolvers/user/update-user.ts
+++ server/graphql/resolvers/user/update-user.ts
@@ -9,19 +9,10 @@ export const updateUser = {
       relations: ['domain', 'roles']
     })
 
-    const roleIds = user.roles.map(role => role.id)
-    if (patch.roles && patch.roles.length) {
-      patch.roles.forEach((roleId: string) => {
-        if (!roleIds.includes(roleId)) {
-          roleIds.push(roleId)
-        }
-      })
-    }
-
     return await repository.save({
       ...user,
       ...patch,
-      roles: await getRepository(Role).findByIds(roleIds),
+      roles: await getRepository(Role).findByIds(patch.roles.map(role => role.id)),
       updaterId: context.state.user.id
     })
   }
