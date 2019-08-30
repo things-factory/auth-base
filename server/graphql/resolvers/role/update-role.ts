035b8353d3f9656400baeb8709c39541766a6d42
horwengliang <horwengliang@gmail.com>
2019-08-30 17:17:05 +0900

    v0.1.85
    remove users in updateRole

--- server/graphql/resolvers/role/update-role.ts
+++ server/graphql/resolvers/role/update-role.ts
@@ -1,22 +1,13 @@
-import { getRepository, In } from 'typeorm'
-import { Priviledge, Role, User } from '../../../entities'
+import { getRepository } from 'typeorm'
+import { Priviledge, Role } from '../../../entities'
 
 export const updateRole = {
   async updateRole(_: any, { name, patch }, context: any) {
     const repository = getRepository(Role)
     const role = await repository.findOne({
       where: { domain: context.domain, name },
-      relations: ['domain', 'users', 'priviledges', 'creator', 'updater']
+      relations: ['domain', 'priviledges', 'creator', 'updater']
     })
-
-    const userEmails = role.users.map(user => user.email)
-    if (patch.users && patch.users.length) {
-      patch.users.forEach((userEmail: string) => {
-        if (!userEmails.includes(userEmail)) {
-          userEmails.push(userEmail)
-        }
-      })
-    }
 
     const priviledgeIds = role.priviledges.map(priviledge => priviledge.id)
     if (patch.priviledges && patch.priviledges.length) {
@@ -30,7 +21,6 @@ export const updateRole = {
     return await repository.save({
       ...role,
       ...patch,
-      users: await getRepository(User).find({ email: In(userEmails) }),
       priviledges: await getRepository(Priviledge).findByIds(priviledgeIds),
       updater: context.state.user
     })
