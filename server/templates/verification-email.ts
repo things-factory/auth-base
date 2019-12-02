58cafe2873ac7b11fe93ee97584b7447fe335ced
horwengliang <horwengliang@gmail.com>
2019-12-02 21:28:28 +0800

    v1.0.0-alpha.20

--- server/templates/verification-email.ts
+++ server/templates/verification-email.ts
@@ -5,7 +5,7 @@ export function getVerificationEmailForm({ name, verifyUrl }) {
       <meta charset="utf-8" />
 
       <title>Verify your email</title>
-      <meta name="description" content="Verify your email" />
+      <meta name="description" content="Email Verification" />
       <meta name="author" content="hatiolab" />
     </head>
 
@@ -33,24 +33,16 @@ export function getVerificationEmailForm({ name, verifyUrl }) {
             >Hi ${name}!</span
           >
           <span style="display:block;color:#fff;font-size:34px;font-weight:bold"
-            >Verify Your Email Address.</span
+            >Verify your email</span
           >
         </div>
         <!--title end-->
 
         <!--body begin-->
         <p style="padding:10px 20px;line-height:1.5;font-size:16px">
-          Your GitHub account was successfully signed in to but we did not
-          recognize the location of the sign in. You can review this sign in
-          attempt by visiting
-          https://github.com/settings/sessions/authentications/294834505. If you
-          recently signed in to your account, you do not need to take any further
-          action. If you did not sign in to your account, your password may be
-          compromised. Visit https://github.com/settings/admin to create a new,
-          strong password for your GitHub account. For more information, see
-          "https://help.github.com/articles/keeping-your-account-and-data-secure/"
-          in the GitHub Help. To see this and other security events for your
-          account, visit https://github.com/settings/security<br />
+          You're almost ready to start enjoying Operato. 
+          Simply click the button below to verify your email address.
+          <br />
           <a
             href="${verifyUrl}"
             style="display:inline-block;margin:10px 5px 5px 0;border-radius:7px;background-color:#22a6a7;padding:7px 15px;color:#fff;font-size:18px;text-decoration:none;text-transform:capitalize;"
