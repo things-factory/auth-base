629bfdeb064c3b1b646d1fdd8302163f0f7a8e28
Murky <moosung.gil@hatiolab.com>
2019-12-13 17:37:18 +0900

    401에 의해 signin으로 갈 때 redirect_to 누락문제 수정

--- server/routes.ts
+++ server/routes.ts
@@ -31,7 +31,10 @@ function getDefaultDomain(userInfo, fallbackUrl = '/domain-select') {
 async function domainCheck(context, next) {
   try {
     const token = getToken(context)
-    if (!token) return context.redirect('/signin')
+    if (!token)
+      throw new AuthError({
+        errorCode: AuthError.ERROR_CODES.TOKEN_INVALID
+      })
     const decodedToken = await User.check(token)
     const { params } = context
     const { domainName } = params
@@ -43,12 +46,16 @@ async function domainCheck(context, next) {
       relations: ['domain', 'domains']
     })
 
-    if (!user) return context.redirect('/signin')
+    if (!user)
+      throw new AuthError({
+        errorCode: AuthError.ERROR_CODES.USER_NOT_FOUND
+      })
     if (!user.domain || user.domain.subdomain != domainName) return context.redirect(`/checkin/${domainName}`)
 
     return next()
   } catch (e) {
-    return context.redirect('/signin')
+    const { originalUrl } = context
+    return context.redirect(`/signin?redirect_to=${originalUrl}`)
   }
 }
 
